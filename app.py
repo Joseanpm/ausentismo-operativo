091618604f780d156f532c17478b799e051bca4c
Joseanpm <148182770+Joseanpm@users.noreply.github.com>
2026-07-03 11:59:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import date
-import plotly.express as px
+import plotly as px
 
 # ─── FIX SSL RED CORPORATIVA DANONE ────────────────────────────────────────
 import httpx
